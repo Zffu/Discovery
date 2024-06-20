8c02e65d39d69416a2bb9f94b501ddda57dbaa4e
Zffu <radi0o0@outlook.com>
2024-06-20 21:28:14 +0200

    Added constructor

--- src/main/java/net/zffu/discovery/items/DiscoveryItemHolder.java
+++ src/main/java/net/zffu/discovery/items/DiscoveryItemHolder.java
@@ -1,11 +1,24 @@
 package net.zffu.discovery.items;
 
+import org.bukkit.inventory.ItemStack;
+
 /**
  * Holds the items of a Gui to allow for static usage.
  * @since 0.0.1
  */
 public class DiscoveryItemHolder {
 
+    /**
+     * The items stored into the holder.
+     */
+    private ItemStack[] items;
 
+    /**
+     * Creates a basic {@link DiscoveryItemHolder}.
+     * @param slots
+     */
+    public DiscoveryItemHolder(int slots) {
+        this.items = new ItemStack[slots];
+    }
 
 }
