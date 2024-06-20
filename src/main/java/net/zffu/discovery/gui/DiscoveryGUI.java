1b69282012334189093c8e71e62483e0e985f749
Zffu <radi0o0@outlook.com>
2024-06-20 21:29:33 +0200

    Added empty gui class

---
+++ src/main/java/net/zffu/discovery/gui/DiscoveryGUI.java
@@ -0,0 +1,10 @@
+package net.zffu.discovery.gui;
+
+/**
+ * The main class of GUIS in Discovery.
+ * @since 0.0.1
+ * @see {@link net.zffu.discovery.items.DiscoveryItemHolder}
+ */
+public abstract class DiscoveryGUI {
+
+}
