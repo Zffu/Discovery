e4b243c3351a9ffb39cc8cf5881f250f3d087392
Zffu <radi0o0@outlook.com>
2024-06-20 21:30:42 +0200

    Added getItems abstract function

--- src/main/java/net/zffu/discovery/gui/DiscoveryGUI.java
+++ src/main/java/net/zffu/discovery/gui/DiscoveryGUI.java
@@ -1,10 +1,19 @@
 package net.zffu.discovery.gui;
 
+import org.bukkit.inventory.ItemStack;
+
 /**
  * The main class of GUIS in Discovery.
  * @since 0.0.1
  * @see {@link net.zffu.discovery.items.DiscoveryItemHolder}
  */
 public abstract class DiscoveryGUI {
+
+    /**
+     * Gets the items of the GUI.
+     * @return
+     */
+    public abstract ItemStack[] getItems();
+
 
 }
